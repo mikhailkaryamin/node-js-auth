25b2da2444f0fbb48db462797d20b6ac5e819f73
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-15 22:00:18 +0500

    feat(server): add db and initial

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log("Drop and Resync Db");
+  initial();
+})
+
 app.get("/", (req, res) => {
   res.json({ 
     message: "Welcome my first Node.js app"
@@ -23,6 +31,24 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 });
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  }),
+
+  Role.create({
+    id: 2,
+    name: "moderator"
+  }),
+
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+};
