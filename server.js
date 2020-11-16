e1f485b8e413a41fa5d4e13b6ad214b9c22adbfd
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-16 23:46:26 +0500

    fix(setver.js): fix cors, remove dev setting

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8001"
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -17,12 +13,8 @@ app.use(bodyParser.urlencoded({
 }));
 
 const db = require("./app/models");
-const Role = db.role;
 
-db.sequelize.sync({force: true}).then(() => {
-  console.log("Drop and Resync Db");
-  initial();
-})
+db.sequelize.sync();
 
 app.get("/", (req, res) => {
   res.json({ 
@@ -30,25 +22,11 @@ app.get("/", (req, res) => {
   });
 });
 
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 });
-
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user"
-  }),
-
-  Role.create({
-    id: 2,
-    name: "moderator"
-  }),
-
-  Role.create({
-    id: 3,
-    name: "admin"
-  });
-};
